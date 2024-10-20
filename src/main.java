5b3aee30acbdad49d253f1c2a0a2ef9a1cc4849d
bgmnn <iamsaltgawd@gmail.com>
2024-10-20 14:18:29 +0300

    changed req minimum length of the passwd

--- src/main.java
+++ src/main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static int minLen = 8;
+    public static int minLen = 16;
     public static int minNum = 1;
     public static int maxNum = 100;
 
@@ -11,22 +11,22 @@ public class Main {
         Scanner input = new Scanner(System.in);
 
         System.out.print("Enter password length (ideally > 22): ");
-        int passwdLen = scanner.nextInt();
+        int passwdLen = input.nextInt();
 
         if (passwdLen < minLen) {
             System.out.println("Password length must be at least " + minLen + ".");
             System.out.print("Enter password length (ideally > 22): ");
-            int passwdLen = scanner.nextInt();
+            int passwdLen = input.nextInt();
         }
 
         System.out.print("How many passwords: ");
-        int passwdNum = scanner.nextInt();
+        int passwdNum = input.nextInt();
 
         if (passwdNum < minNum) {
             System.out.println("Invalid number. The amount was set to " + minNum + ".");
             passwdNum = 1;
         }   else if (passwdNum > maxNum) {
-            System.out.println("The amount was set to " + maxNum + " (capped at 100)")
+            System.out.println("The amount was set to " + maxNum + " (capped at 100)");
             }
 
         System.out.println("Generating " + passwdNum + " passwords: ");
@@ -36,7 +36,7 @@ public class Main {
             System.out.println((i+1) + ": " + passwd);
         }
 
-        scanner.close();
+        input.close();
     }
 
 }
